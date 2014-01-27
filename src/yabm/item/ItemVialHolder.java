54eae024860aa3efe1b360ac54a5edfa5fc10a65
Kihira <zoe.kihira.lee@gmail.com>
2014-01-27 23:47:18 +0000

    Beginning of auto casting from vial holder Fire resistance is the only one that currently works

--- src/yabm/item/ItemVialHolder.java
+++ src/yabm/item/ItemVialHolder.java
@@ -4,10 +4,17 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import yabm.YABM;
 import yabm.api.IBackpack;
+import yabm.inventory.InventoryNBT;
+import yabm.util.NBTHelper;
+
+import java.util.List;
 
 public class ItemVialHolder extends Item implements IBackpack {
 
@@ -23,7 +30,37 @@ public class ItemVialHolder extends Item implements IBackpack {
         return itemStack;
     }
 
-    public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
+    public void onUpdate(ItemStack itemStack, World world, Entity entity, int inventorySlot, boolean currentItem) {
         //TODO Auto consume potions based on player stats
+        if (!world.isRemote && itemStack.hasTagCompound()) {
+            EntityPlayer player = (EntityPlayer) entity;
+            if (entity.isBurning()) {
+                ItemStack[] vialHolderInventory = NBTHelper.loadInventory(itemStack.getTagCompound(), 9);
+                itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 12)));
+            }
+        }
+    }
+
+    //Returns an updated array of the vial holder's inventory.
+    private ItemStack[] applyPotionEffectIfFound(ItemStack[] vialHolderInventory, EntityPlayer player, int potionID) {
+        for (int i = 0; i < vialHolderInventory.length; i++) {
+            boolean flag = false;
+            if (vialHolderInventory[i] != null) {
+                ItemPotion itemPotion = (ItemPotion) vialHolderInventory[i].getItem();
+                List potionEffects = itemPotion.getEffects(vialHolderInventory[i]);
+                for (Object object:potionEffects) {
+                    PotionEffect potionEffect = (PotionEffect) object;
+                    if (potionEffect.getPotionID() == potionID) {
+                        player.addPotionEffect(potionEffect);
+                        player.playSound("random.drink", 0.5F, player.worldObj.rand.nextFloat() * 0.1F + 0.9F);
+                        vialHolderInventory[i] = null;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if (flag) break;
+        }
+        return vialHolderInventory;
     }
 }
---
+++ src/yabm/util/NBTHelper.java
@@ -0,0 +1,36 @@
+package yabm.util;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+
+public class NBTHelper {
+
+    public static ItemStack[] loadInventory(NBTTagCompound tagCompound, int size) {
+        NBTTagList nbtTagList = tagCompound.getTagList("Contents");
+        ItemStack[] itemStacks = new ItemStack[size];
+
+        for (int i = 0; i < nbtTagList.tagCount(); i++) {
+            NBTTagCompound nbttagcompound1 = (NBTTagCompound)nbtTagList.tagAt(i);
+            int j = nbttagcompound1.getByte("Slot") & 255;
+            if (j >= 0 && j < size) itemStacks[j] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+        }
+        return itemStacks;
+    }
+
+    public static NBTTagCompound writeInventory(ItemStack[] inventory) {
+        NBTTagList nbttaglist = new NBTTagList();
+        NBTTagCompound tagCompound = new NBTTagCompound();
+
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i] != null) {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte) i);
+                inventory[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+        tagCompound.setTag("Contents", nbttaglist);
+        return tagCompound;
+    }
+}
