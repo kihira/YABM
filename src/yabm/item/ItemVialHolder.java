fadab6d4064d94dc0ccfc68d34d5e54d7469b6f3
Kihira <zoe.kihira.lee@gmail.com>
2014-01-28 13:22:11 +0000

    More potion auto casting! Adds swift, jump, haste, health, water breathing and resistance

--- src/yabm/item/ItemVialHolder.java
+++ src/yabm/item/ItemVialHolder.java
@@ -2,11 +2,13 @@ package yabm.item;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import yabm.YABM;
@@ -31,13 +33,21 @@ public class ItemVialHolder extends Item implements IBackpack {
     }
 
     public void onUpdate(ItemStack itemStack, World world, Entity entity, int inventorySlot, boolean currentItem) {
-        //TODO Auto consume potions based on player stats
-        if (!world.isRemote && itemStack.hasTagCompound()) {
+        if (!world.isRemote && world.getTotalWorldTime() % 10 == 0 && itemStack.hasTagCompound()) {
             EntityPlayer player = (EntityPlayer) entity;
-            if (entity.isBurning()) {
-                ItemStack[] vialHolderInventory = NBTHelper.loadInventory(itemStack.getTagCompound(), 9);
-                itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 12)));
+            ItemStack[] vialHolderInventory = NBTHelper.loadInventory(itemStack.getTagCompound(), 9);
+            if (entity.isBurning()) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 12)));
+            if (player.getMaxHealth() / player.getHealth() > 4) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 6)));
+            if (player.getAir() < 50) {
+                player.setAir(300);
+                itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 13)));
             }
+            //Only apply resistance if attack attacked within the last second and has lost more then one heart of damage. Should prevent accidental casting.
+            if ((player.getLastAttackerTime() < 20) && (player.getMaxHealth() - player.getHealth() > 1)) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 11)));
+            //Only apply sprint/jump/haste if player currently has sprint as idk when else to trigger this
+            if ((player.getActivePotionEffect(Potion.moveSpeed) != null) && (player.getActivePotionEffect(Potion.moveSpeed).getDuration() < 50)) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 1)));
+            if ((player.getActivePotionEffect(Potion.jump) != null) && (player.getActivePotionEffect(Potion.jump).getDuration() < 50)) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 8)));
+            if ((player.getActivePotionEffect(Potion.digSpeed) != null) && (player.getActivePotionEffect(Potion.digSpeed).getDuration() < 50)) itemStack.setTagCompound(NBTHelper.writeInventory(applyPotionEffectIfFound(vialHolderInventory, player, 3)));
         }
     }
 
@@ -52,7 +62,7 @@ public class ItemVialHolder extends Item implements IBackpack {
                     PotionEffect potionEffect = (PotionEffect) object;
                     if (potionEffect.getPotionID() == potionID) {
                         player.addPotionEffect(potionEffect);
-                        player.playSound("random.drink", 0.5F, player.worldObj.rand.nextFloat() * 0.1F + 0.9F);
+                        player.worldObj.playSoundAtEntity(player, "random.drink", 0.5F, player.worldObj.rand.nextFloat() * 0.1F + 0.9F);
                         vialHolderInventory[i] = null;
                         flag = true;
                         break;
